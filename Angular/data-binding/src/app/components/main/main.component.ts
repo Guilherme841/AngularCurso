1b3de62cf2ff05804140f1a2f5659ae0681bb529
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2023-12-22 21:58:54 -0300

    update

--- Angular/data-binding/src/app/components/main/main.component.ts
+++ Angular/data-binding/src/app/components/main/main.component.ts
@@ -11,12 +11,19 @@ export class MainComponent {
   order: boolean = true;
   urlImage: any =
     'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI';
-
+  keyBoardValue: string = '';
   getOrder() {
     return true;
   }
 
   getValor() {
     return 5;
+  }
+  alert() {
+    alert('Ok');
+  }
+  onKeyUp(event: KeyboardEvent) { // Posso tipar a variavel pra usar Intelligence .Tal
+    this.keyBoardValue += event.key; // isso é o mesmo que o de baixo
+    // this.keyBoardValue = ((<HTMLInputElement>event.target).value);
   }
 }
