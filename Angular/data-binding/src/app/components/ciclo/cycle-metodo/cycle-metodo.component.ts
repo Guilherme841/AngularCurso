906fa3885771b4c4b2a9e55cfdaea359b248d916
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2023-12-29 19:50:07 -0300

    update

--- Angular/data-binding/src/app/components/ciclo/cycle-metodo/cycle-metodo.component.ts
+++ Angular/data-binding/src/app/components/ciclo/cycle-metodo/cycle-metodo.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  OnDestroy,
+  DoCheck,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  AfterViewChecked,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cycle-metodo',
@@ -7,9 +17,42 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './cycle-metodo.component.html',
   styleUrl: './cycle-metodo.component.css',
 })
-export class CycleMetodoComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {
-    console.log(this.ngOnInit);
+export class CycleMetodoComponent
+  implements
+    OnInit,
+    OnChanges,
+    OnDestroy,
+    DoCheck,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewInit,
+    AfterViewChecked
+{
+  constructor() {
+    console.log('construtor');
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(): void {
+    console.log('OnChanges');
+  }
+  ngOnDestroy(): void {
+    console.log('OnDestroy');
+  }
+  ngDoCheck(): void {
+    console.log('doCheck');
+  }
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit');
+  }
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked');
   }
 }
