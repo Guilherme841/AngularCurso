b0b67e95d0c7f1de14bc6465c79445fccd65f80c
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2023-12-31 16:04:41 -0300

    update

---
+++ Angular/diretivas/src/app/components/diretiva-if/diretiva-if.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-if',
+  standalone: true,
+  imports: [],
+  templateUrl: './diretiva-if.component.html',
+  styleUrl: './diretiva-if.component.css'
+})
+export class DiretivaIfComponent {
+
+}
