97577421113dd294cf036ef0613571c1112dd0d2
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2024-01-07 18:24:05 -0300

    update

--- Angular/diretivas/src/app/components/diretivas-atributivas/diretiva-class/diretiva-class.component.ts
+++ Angular/diretivas/src/app/components/diretivas-atributivas/diretiva-class/diretiva-class.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-diretiva-class',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './diretiva-class.component.html',
   styleUrl: './diretiva-class.component.scss'
 })
