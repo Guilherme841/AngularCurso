226c8df18b6dd186bd867a6ee8febc0ac448a6db
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2024-01-08 20:29:52 -0300

    update

--- Angular/diretivas/src/app/components/diretivas-atributivas/diretiva-style/diretiva-style.component.ts
+++ Angular/diretivas/src/app/components/diretivas-atributivas/diretiva-style/diretiva-style.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './diretiva-style.component.html',
-  styleUrl: './diretiva-style.component.scss'
+  styleUrl: './diretiva-style.component.scss',
 })
 export class DiretivaStyleComponent {
-
+  buttonOn: boolean = false;
+  fontSize: number = 1;
 }
