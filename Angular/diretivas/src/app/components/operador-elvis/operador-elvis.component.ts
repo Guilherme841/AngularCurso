95086553cdc9575f00c7ee65e0844bdcd6331e45
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2024-01-09 16:37:59 -0300

    updated, created pessoa object

--- Angular/diretivas/src/app/components/operador-elvis/operador-elvis.component.ts
+++ Angular/diretivas/src/app/components/operador-elvis/operador-elvis.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './operador-elvis.component.html',
-  styleUrl: './operador-elvis.component.scss'
+  styleUrl: './operador-elvis.component.scss',
 })
 export class OperadorElvisComponent {
-
+  pessoa: object = {
+    name: 'Fulano',
+    age: 20,
+    address: {
+      street: 'qualquer',
+      number: 76,
+    },
+  };
 }
