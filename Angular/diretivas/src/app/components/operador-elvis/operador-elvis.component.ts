56c496faf4d781f65ece747fddaadf6df9ff6bcc
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2024-01-09 16:21:28 -0300

    update

---
+++ Angular/diretivas/src/app/components/operador-elvis/operador-elvis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operador-elvis',
+  standalone: true,
+  imports: [],
+  templateUrl: './operador-elvis.component.html',
+  styleUrl: './operador-elvis.component.scss'
+})
+export class OperadorElvisComponent {
+
+}
