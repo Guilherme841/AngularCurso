4129639db6a3e5ff7200a481b06810d847819094
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-01-13 18:36:19 -0300

    update

--- Angular/directives/src/app/directives/structural-directive/else.directive.ts
+++ Angular/directives/src/app/directives/structural-directive/else.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
-
+// TemplateRef armazena um template em uma variável para criar uma nova view.
+// ViewContainerRef marca um lugar para a view ser exibida/inserida.
 @Directive({
   selector: '[appElse]',
   standalone: true,
@@ -9,8 +10,12 @@ export class ElseDirective {
   // Input property para expor o método
   @Input() set appElse(condition: boolean) {
     if (!condition) {
+      // Cria uma nova visualização dentro do contêiner especificado, usando o template armazenado na variável this._TemplateRef.
       this._ViewContainerRef.createEmbeddedView(this._TemplateRef);
     } else {
+      // Esta linha de código remove todas as visualizações embutidas
+      // (embedded views) que estão atualmente presentes dentro do contêiner de
+      // visualizações especificado por this._ViewContainerRef.
       this._ViewContainerRef.clear();
     }
   }
