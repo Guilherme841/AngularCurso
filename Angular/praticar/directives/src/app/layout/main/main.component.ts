cd601d6e5ccad43e9dc19a79cce5eb846f81fbd9
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-01-14 22:27:02 -0300

    update

--- Angular/praticar/directives/src/app/layout/main/main.component.ts
+++ Angular/praticar/directives/src/app/layout/main/main.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { DiretivaEstruturalComponent } from '../../components/diretiva-estrutural/diretiva-estrutural.component';
 import { DiretivaAtributoComponent } from '../../components/diretiva-atributo/diretiva-atributo.component';
+import { NgContentComponent } from '../../components/ng-content/ng-content.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [DiretivaEstruturalComponent,DiretivaAtributoComponent],
+  imports: [
+    DiretivaEstruturalComponent,
+    DiretivaAtributoComponent,
+    NgContentComponent,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
