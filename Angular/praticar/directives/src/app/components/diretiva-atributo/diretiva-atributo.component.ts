4312a1fb28ffbf487da70b603dcc5080aa933479
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-01-14 22:53:52 -0300

    update

--- Angular/praticar/directives/src/app/components/diretiva-atributo/diretiva-atributo.component.ts
+++ Angular/praticar/directives/src/app/components/diretiva-atributo/diretiva-atributo.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CustomDirectiveDirective } from '../../directives/custom-directive.directive';
 
 @Component({
   selector: 'app-diretiva-atributo',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, CustomDirectiveDirective],
   templateUrl: './diretiva-atributo.component.html',
   styleUrl: './diretiva-atributo.component.scss',
 })
--- Angular/praticar/directives/src/app/directives/custom-directive.directive.ts
+++ Angular/praticar/directives/src/app/directives/custom-directive.directive.ts
@@ -1,11 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCustomDirective]',
-  standalone: true
+  standalone: true,
 })
 export class CustomDirectiveDirective {
-
-  constructor() { }
-
+  // @Input() backgroundColor!: string;
+  @HostBinding('style.background') backgroundColor!: string;
+  @HostListener('click')
+  clickedDirective() {
+    this.backgroundColor = 'lightskyblue'
+    alert('Diretiva Customizada Funcionando!');
+  }
+  constructor() {}
 }
