1bc549f864fc39eb5062151e42ba16412ff9cd2b
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-01-14 21:39:52 -0300

    update

--- Angular/praticar/directives/src/app/components/diretiva-atributo/diretiva-atributo.component.ts
+++ Angular/praticar/directives/src/app/components/diretiva-atributo/diretiva-atributo.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-diretiva-atributo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './diretiva-atributo.component.html',
   styleUrl: './diretiva-atributo.component.scss',
 })
 export class DiretivaAtributoComponent {
   toggleBoolean: boolean = false;
+  nomeInput!: string;
 }
