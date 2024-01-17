13cee0830e32a3a3091a8af7956df82eccd07a93
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-01-17 17:11:28 -0300

    update

--- Angular/services/src/app/components/create-user/create-user.component.ts
+++ Angular/services/src/app/components/create-user/create-user.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
+import { SearchUsersService } from './../../services/search-users.service';
+
 @Component({
   selector: 'app-create-user',
   standalone: true,
   imports: [],
+  providers: [SearchUsersService],
   templateUrl: './create-user.component.html',
-  styleUrl: './create-user.component.scss'
+  styleUrl: './create-user.component.scss',
 })
 export class CreateUserComponent {
-
+  constructor(private _searchUsersService: SearchUsersService) {
+    console.log(this._searchUsersService.findUsers());
+  }
 }
--- Angular/services/src/app/layout/main/main.component.ts
+++ Angular/services/src/app/layout/main/main.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { UsersComponent } from '../../components/users/users.component';
+import { CreateUserComponent } from '../../components/create-user/create-user.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [UsersComponent],
+  imports: [UsersComponent, CreateUserComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
