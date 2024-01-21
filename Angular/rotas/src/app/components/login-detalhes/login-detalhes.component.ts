046fce4e20ef791863c43308a301659ce105ebbd
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-01-21 10:34:27 -0300

    update

---
+++ Angular/rotas/src/app/components/login-detalhes/login-detalhes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-detalhes',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-detalhes.component.html',
+  styleUrl: './login-detalhes.component.scss'
+})
+export class LoginDetalhesComponent {
+
+}
