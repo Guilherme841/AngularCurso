e5efb311bdb1dffad7bab7cce6663b2a1b0ab361
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-01-22 22:56:39 -0300

    update

--- Angular/rotas/src/app/app.routes.ts
+++ Angular/rotas/src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { LayoutComponent } from './layout/layout/layout.component';
+import { LayoutComponent } from './ui/layout/layout.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
   },
-  // {
-  //   path: ''
-  // }
 ];
