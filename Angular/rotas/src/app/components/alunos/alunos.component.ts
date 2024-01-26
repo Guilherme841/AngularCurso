51462b39734512a3669a12e7c5b163a5f87567c3
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-01-26 16:26:47 -0300

    update

--- Angular/rotas/src/app/components/alunos/alunos.component.ts
+++ Angular/rotas/src/app/components/alunos/alunos.component.ts
@@ -9,14 +9,15 @@ import { AlunosService } from '../../services/alunos.service';
   templateUrl: './alunos.component.html',
   styleUrl: './alunos.component.scss',
 })
-
 export class AlunosComponent implements OnInit {
   protected alunos!: Array<object>;
+  nomeAlunos: Array<string> = [];
   constructor(private _alunosService: AlunosService) {}
   ngOnInit() {
     this.alunos = this._alunosService.getAlunos();
-    let nomeAlunos = this.alunos.find((pessoa: any) => {
-      let {nome}  = pessoa;
+    this.alunos.find((pessoa: any) => {
+      let { nome } = pessoa;
+      this.nomeAlunos.push(nome);
     });
   }
 }
