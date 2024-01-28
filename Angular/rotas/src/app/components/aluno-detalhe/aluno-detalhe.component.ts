86892ca91dfd1edcfd338db152a5cd3c34ba877f
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-01-27 22:50:31 -0300

    update

--- Angular/rotas/src/app/components/aluno-detalhe/aluno-detalhe.component.ts
+++ Angular/rotas/src/app/components/aluno-detalhe/aluno-detalhe.component.ts
@@ -20,6 +20,12 @@ export class AlunoDetalheComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.subscribe = this._activatedRoute.params.subscribe((id) => {
       this.idAluno = id['id'];
+      this._alunosService.getAlunos().forEach((objeto: any) => {
+        if (objeto.nome === this.idAluno) {
+          console.log(objeto);
+          return objeto;
+        }
+      });
     });
   }
   ngOnDestroy(): void {
